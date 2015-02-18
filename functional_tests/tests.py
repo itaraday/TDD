f0b05d1a64826824565645422ccaa6bede41b813
itaraday <ira.taraday.01@gmail.com>
2015-02-18 16:23:47 -0500

    made function test its own folder, using liveservertest now

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		#force browser open for at least 3 seconds
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 	
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#checking that homepage works
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		
 		#notice the page header and tiotle mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
