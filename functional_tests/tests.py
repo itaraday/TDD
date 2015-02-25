11753abb1f06c67c62f29c7e73f24e0b47a29910
itaraday <ira.taraday.01@gmail.com>
2015-02-24 22:28:38 -0500

    added bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		#force browser open for at least 3 seconds
@@ -28,7 +29,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertAlmostEqual(
 			inputbox.location['x'] + inputbox.size['width'] / 2,
 			512,
-			delta=5
+			delta=15
 		)
 		
 		inputbox.send_keys('testing\n')
@@ -36,7 +37,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertAlmostEqual(
 			inputbox.location['x'] + inputbox.size['width'] / 2,
 			512,
-			delta=5
+			delta=15
 		)		
 		
 	
